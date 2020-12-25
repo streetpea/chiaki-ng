aba17d48a32ba42b0d14f298143b1f2a88cada25
Florian Märkl <info@florianmaerkl.de>
2020-12-25 15:04:57 +0100

    Add correct Bitrate for 1080p

--- lib/src/session.c
+++ lib/src/session.c
@@ -108,7 +108,7 @@ CHIAKI_EXPORT void chiaki_connect_video_profile_preset(ChiakiConnectVideoProfile
 		case CHIAKI_VIDEO_RESOLUTION_PRESET_1080p:
 			profile->width = 1920;
 			profile->height = 1080;
-			profile->bitrate = 10000; // TODO
+			profile->bitrate = 15000;
 			break;
 		default:
 			profile->width = 0;
