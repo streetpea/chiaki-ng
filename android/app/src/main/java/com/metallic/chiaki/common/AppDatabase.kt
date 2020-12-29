1ce2dbd5d2f8135561c0e03bcccb296c08f4ac10
Florian Märkl <info@florianmaerkl.de>
2020-12-29 20:11:41 +0100

    Fix Android Database Migration

--- android/app/src/main/java/com/metallic/chiaki/common/AppDatabase.kt
+++ android/app/src/main/java/com/metallic/chiaki/common/AppDatabase.kt
@@ -23,9 +23,11 @@ val MIGRATION_1_2 = object : Migration(1, 2)
 {
 	override fun migrate(database: SupportSQLiteDatabase)
 	{
-		database.execSQL("ALTER TABLE registered_host RENAME ps4_mac TO server_mac")
-		database.execSQL("ALTER TABLE registered_host RENAME ps4_nickname TO server_nickname")
 		database.execSQL("ALTER TABLE registered_host ADD target INTEGER NOT NULL DEFAULT 1000")
+		database.execSQL("CREATE TABLE `new_registered_host` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `target` INTEGER NOT NULL, `ap_ssid` TEXT, `ap_bssid` TEXT, `ap_key` TEXT, `ap_name` TEXT, `server_mac` INTEGER NOT NULL, `server_nickname` TEXT, `rp_regist_key` BLOB NOT NULL, `rp_key_type` INTEGER NOT NULL, `rp_key` BLOB NOT NULL)");
+		database.execSQL("INSERT INTO `new_registered_host` SELECT `id`, `target`, `ap_ssid`, `ap_bssid`, `ap_key`, `ap_name`, `ps4_mac`, `ps4_nickname`, `rp_regist_key`, `rp_key_type`, `rp_key` FROM `registered_host`")
+		database.execSQL("DROP TABLE registered_host")
+		database.execSQL("ALTER TABLE new_registered_host RENAME TO registered_host")
 	}
 }
 
