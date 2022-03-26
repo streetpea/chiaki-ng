aa3a3b8bbcfd1c45a0b10ec6d6d2aa4e3d8bb131
Florian Märkl <info@florianmaerkl.de>
2022-03-26 09:53:38 +0100

    Update Windows Dependencies in CI

--- scripts/appveyor-win.sh
+++ scripts/appveyor-win.sh
@@ -26,7 +26,7 @@ ninja || exit 1
 ninja install || exit 1
 cd ../.. || exit 1
 
-wget https://mirror.firedaemon.com/OpenSSL/openssl-1.1.1l-dev.zip && 7z x openssl-1.1.1l-dev.zip || exit 1
+wget https://mirror.firedaemon.com/OpenSSL/openssl-1.1.1n.zip && 7z x openssl-1.1.1n.zip || exit 1
 
 wget https://www.libsdl.org/release/SDL2-devel-2.0.10-VC.zip && 7z x SDL2-devel-2.0.10-VC.zip || exit 1
 export SDL_ROOT="$APPVEYOR_BUILD_FOLDER/SDL2-2.0.10" || exit 1
@@ -43,7 +43,7 @@ export PATH="$PWD/protoc/bin:$PATH" || exit 1
 PYTHON="C:/Python37/python.exe"
 "$PYTHON" -m pip install protobuf || exit 1
 
-QT_PATH="C:/Qt/5.12/msvc2017_64"
+QT_PATH="C:/Qt/5.15/msvc2019_64"
 
 COPY_DLLS="$PWD/openssl-1.1/x64/bin/libcrypto-1_1-x64.dll $PWD/openssl-1.1/x64/bin/libssl-1_1-x64.dll $SDL_ROOT/lib/x64/SDL2.dll"
 
