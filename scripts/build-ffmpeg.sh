8b6870e716fe5a456f66351631cb876dd503ef83
Street Pea <streetpea@proton.me>
2023-12-09 11:17:43 -0800

    Add ffmpeg patch to build ffmpeg script

--- scripts/build-ffmpeg.sh
+++ scripts/build-ffmpeg.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $(dirname "${BASH_SOURCE[0]}")/..
 cd "./$1"
 shift
@@ -8,7 +9,7 @@ ROOT="`pwd`"
 TAG=n6.1
 
 git clone https://git.ffmpeg.org/ffmpeg.git --depth 1 -b $TAG && cd ffmpeg || exit 1
-
+git apply ${SCRIPT_DIR}/flatpak/0001-hevc-Fix-duplicate-POC-in-sequence.patch || exit 1
 ./configure --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_vaapi --enable-hwaccel=hevc_vaapi --enable-hwaccel=h264_vulkan --enable-hwaccel=hevc_vulkan --prefix="$ROOT/ffmpeg-prefix" "$@" || exit 1
 make -j4 || exit 1
 make install || exit 1
