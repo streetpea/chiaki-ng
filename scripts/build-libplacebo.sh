218ff09f3966bcadbc5b9b3e7813840ab8a0c842
Streetpea <streetpea@proton.me>
2024-01-10 12:18:38 -0800

    Add ffmpeg and libplacebo patches to appimage build

--- scripts/build-libplacebo.sh
+++ scripts/build-libplacebo.sh
@@ -12,6 +12,8 @@ git clone --recursive https://github.com/haasn/libplacebo.git -b $TAG || exit 1
 fi
 cd libplacebo || exit 1
 git apply ${SCRIPT_DIR}/flatpak/0001-Vulkan-Don-t-try-to-reuse-old-swapchain.patch || exit 1
+git apply ${SCRIPT_DIR}/flatpak/libplacebo-pr238.patch || exit 1
+git apply ${SCRIPT_DIR}/flatpak/libplacebo-pr239.patch || exit 1
 DIR=./build || exit 1
 meson setup --prefix /usr -Dxxhash=disabled $DIR || exit 1
 ninja -C$DIR || exit 1
