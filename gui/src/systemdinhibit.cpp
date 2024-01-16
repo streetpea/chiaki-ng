22dd083d612074449478ec3396d434c39da240fd
David Rosca <nowrep@gmail.com>
2024-01-16 07:49:46 +0100

    Set CHIAKI_HAVE_DBUS when building with QtDBus

--- gui/src/systemdinhibit.cpp
+++ gui/src/systemdinhibit.cpp
@@ -1,6 +1,6 @@
 #include "systemdinhibit.h"
 
-#if defined(QT_DBUS_LIB) && !defined(Q_OS_WIN)
+#ifdef CHIAKI_HAVE_DBUS
 #include <fcntl.h>
 #include <unistd.h>
 #include <QDBusMessage>
@@ -17,7 +17,7 @@ SystemdInhibit::SystemdInhibit(const QString &who, const QString &why, const QSt
     , what(what)
     , mode(mode)
 {
-#if defined(QT_DBUS_LIB) && !defined(Q_OS_WIN)
+#ifdef CHIAKI_HAVE_DBUS
     QDBusConnection::systemBus().connect(QStringLiteral("org.freedesktop.login1"),
                                          QStringLiteral("/org/freedesktop/login1"),
                                          QStringLiteral("org.freedesktop.login1.Manager"),
@@ -29,7 +29,7 @@ SystemdInhibit::SystemdInhibit(const QString &who, const QString &why, const QSt
 
 void SystemdInhibit::inhibit()
 {
-#if defined(QT_DBUS_LIB) && !defined(Q_OS_WIN)
+#ifdef CHIAKI_HAVE_DBUS
     QDBusMessage call = QDBusMessage::createMethodCall(QStringLiteral("org.freedesktop.login1"),
                                                        QStringLiteral("/org/freedesktop/login1"),
                                                        QStringLiteral("org.freedesktop.login1.Manager"),
@@ -63,7 +63,7 @@ void SystemdInhibit::login1PrepareForSleep(bool start)
 
 void SystemdInhibit::release()
 {
-#if defined(QT_DBUS_LIB) && !defined(Q_OS_WIN)
+#ifdef CHIAKI_HAVE_DBUS
     if (fd >= 0)
         close(fd);
     fd = -1;
