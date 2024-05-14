183241c55cf5168c6e0bbc575f7b381b18174ce6
Street Pea <streetpea@proton.me>
2024-05-13 18:05:21 -0700

    Update msys2 build script

--- scripts/build-ffmpeg-windows.sh
+++ scripts/build-ffmpeg-windows.sh
@@ -10,6 +10,6 @@ TAG=n7.0
 
 git clone https://git.ffmpeg.org/ffmpeg.git --depth 1 -b $TAG && cd ffmpeg || exit 1
 git apply ${SCRIPT_DIR}/flatpak/0001-vulkan-ignore-frames-without-hw-context.patch || exit 1
-./configure --disable-all --enable-shared --enable-avformat --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_vulkan --enable-hwaccel=hevc_vulkan --prefix=/usr/local "$@" || exit 1
+./configure --disable-all --enable-shared --enable-avformat --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_vulkan --enable-hwaccel=hevc_vulkan --enable-hwaccel=h264_d3d11va --enable-hwaccel=hevc_d3d11va --prefix=/mingw64 "$@" || exit 1
 make -j4 || exit 1
 make install || exit 1
