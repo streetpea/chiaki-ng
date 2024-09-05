fc884fa5c99feb4028bb4a5a7fa046888563aff8
Streetpea <streetpea@proton.me>
2024-09-05 13:54:38 -0700

    Update to libplacebo v7.349.0

--- scripts/build-libplacebo.sh
+++ scripts/build-libplacebo.sh
@@ -6,7 +6,7 @@ cd "./$1"
 shift
 ROOT="`pwd`"
 
-TAG=c320f61e601caef2be081ce61138e5d51c1be21d
+TAG=v7.349.0
 if [ ! -d "libplacebo" ]; then
 git clone --recursive https://github.com/haasn/libplacebo.git || exit 1
 fi
@@ -14,6 +14,6 @@ cd libplacebo || exit 1
 git checkout $TAG || exit 1
 git apply ${SCRIPT_DIR}/flatpak/0001-Vulkan-Don-t-try-to-reuse-old-swapchain.patch || exit 1
 DIR=./build || exit 1
-meson setup --prefix /usr -Dxxhash=disabled $DIR || exit 1
+meson setup --prefix /usr/local -Dxxhash=disabled $DIR || exit 1
 ninja -C$DIR || exit 1
 ninja -Cbuild install || exit 1
