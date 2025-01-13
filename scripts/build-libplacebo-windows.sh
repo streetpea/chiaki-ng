be7568dfdd7c011c3fb0ff55d7a99695aa5f37ec
Street Pea <streetpea@proton.me>
2025-01-12 18:04:22 -0800

    Update build scripts some more

--- scripts/build-libplacebo-windows.sh
+++ scripts/build-libplacebo-windows.sh
@@ -12,7 +12,7 @@ git clone --recursive https://github.com/haasn/libplacebo.git || exit 1
 fi
 cd libplacebo || exit 1
 git checkout $TAG || exit 1
-#git apply "${SCRIPT_DIR}/flatpak/0002-Vulkan-use-16bit-for-p010.patch" | exit 1
+git apply "${SCRIPT_DIR}/flatpak/0002-Vulkan-use-16bit-for-p010.patch" | exit 1
 DIR=./build || exit 1
 meson setup --prefix /mingw64 -Dxxhash=disabled $DIR || exit 1
 ninja -C$DIR || exit 1
