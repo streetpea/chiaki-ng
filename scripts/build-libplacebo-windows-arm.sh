c95fa6237ab763239a7c7e9325d406168dbe0994
Street Pea <streetpea@proton.me>
2025-04-20 09:25:15 -0700

    Update nanopb to 0.4.9.1

---
+++ scripts/build-libplacebo-windows-arm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $(dirname "${BASH_SOURCE[0]}")/..
+cd "./$1"
+shift
+ROOT="`pwd`"
+
+TAG=v7.349.0
+if [ ! -d "libplacebo" ]; then
+git clone --recursive https://github.com/haasn/libplacebo.git || exit 1
+fi
+cd libplacebo || exit 1
+git checkout $TAG || exit 1
+git apply "${SCRIPT_DIR}/flatpak/0002-Vulkan-use-16bit-for-p010.patch" | exit 1
+DIR=./build || exit 1
+meson setup --prefix /clangarm64 -Dxxhash=disabled $DIR || exit 1
+ninja -C$DIR || exit 1
+ninja -Cbuild install || exit 1
