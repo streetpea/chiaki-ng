a95cda62fe874ddec234628abe6159a349db5356
Street Pea <streetpea@proton.me>
2025-06-08 12:30:44 -0700

    Use latest sdl3 commit for revolution unlimited x controller

--- scripts/build-sdl2-compat.sh
+++ scripts/build-sdl2-compat.sh
@@ -6,16 +6,32 @@ cd $(dirname "${BASH_SOURCE[0]}")/..
 cd "./$1"
 ROOT="`pwd`"
 
-SDL_VER=3.2.16
 SDL_COMPAT_VER=2.32.56
-URL=https://github.com/libsdl-org/SDL/releases/download/release-${SDL_VER}/SDL3-${SDL_VER}.tar.gz
-FILE=SDL3-${SDL_VER}.tar.gz
-DIR=SDL3-${SDL_VER}
+# URL=https://github.com/libsdl-org/SDL/releases/download/release-${SDL_VER}/SDL3-${SDL_VER}.tar.gz
+# SDL_VER=3.2.16
+# FILE=SDL3-${SDL_VER}.tar.gz
+# DIR=SDL3-${SDL_VER}
 
-if [ ! -d "$DIR" ]; then
-	curl -L "$URL" -O
-	tar -xf "$FILE"
-fi
+# if [ ! -d "$DIR" ]; then
+# 	curl -L "$URL" -O
+# 	tar -xf "$FILE"
+# fi
+DIR=SDL
+git clone https://github.com/libsdl-org/SDL ${DIR}
+
+cd "$DIR" || exit 1
+INSTALL_PREFIX="${INSTALL_PREFIX:=/usr}"
+mkdir -p build && cd build || exit 1
+cmake \
+	-DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
+	-DCMAKE_PREFIX_PATH="$PWD/../../$DIR" \
+	-DSDL_TESTS=OFF \
+	-DSDL_EXAMPLES=OFF \
+	..
+
+make -j4
+make install
+cd ../..
 
 URL=https://github.com/libsdl-org/sdl2-compat/releases/download/release-${SDL_COMPAT_VER}/sdl2-compat-${SDL_COMPAT_VER}.tar.gz
 FILE=sdl2-compat-${SDL_COMPAT_VER}.tar.gz
