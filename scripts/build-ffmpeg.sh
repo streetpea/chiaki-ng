0116b992dfcf6e1bb3e5de847e631edb03769f10
Street Pea <streetpea@proton.me>
2025-06-22 09:06:39 -0700

    Add /usr/include to CPATH

--- scripts/build-ffmpeg.sh
+++ scripts/build-ffmpeg.sh
@@ -10,6 +10,7 @@ TAG=n7.1
 
 git clone https://git.ffmpeg.org/ffmpeg.git --depth 1 -b $TAG && cd ffmpeg || exit 1
 git apply ${SCRIPT_DIR}/flatpak/0001-vulkan-ignore-frames-without-hw-context.patch || exit 1
-./configure --list-hwaccels --disable-all --enable-static --enable-avformat --enable-vulkan --enable-libv4l2 --enable-libdrm --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_vaapi --enable-hwaccel=hevc_vaapi --enable-hwaccel=h264_vulkan --enable-hwaccel=hevc_vulkan --enable-hwaccel=h264_v4l2m2m --enable-hwaccel=hevc_v4l2m2m --prefix=/usr "$@" || exit 1
+CPATH="/usr/include"
+./configure --list-hwaccels --disable-all --enable-static --enable-avformat --enable-vulkan --disable-libv4l2_m2m=false --enable-libv4l2 --enable-libdrm --enable-avcodec --enable-v4l2_m2m --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_vaapi --enable-hwaccel=hevc_vaapi --enable-hwaccel=h264_vulkan --enable-hwaccel=hevc_vulkan --enable-hwaccel=h264_v4l2m2m --enable-hwaccel=hevc_v4l2m2m --prefix=/usr "$@" || exit 1
 make -j4 || exit 1
 make install || exit 1
