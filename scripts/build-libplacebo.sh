721938f7d8ec7f4e77f5e470e7d9033bd9de4c97
Street Pea <streetpea@proton.me>
2026-01-22 05:26:17 -0800

    Use newer libplacebo commit that fixes several issues + remove recreate swapchain patch

--- scripts/build-libplacebo.sh
+++ scripts/build-libplacebo.sh
@@ -6,14 +6,12 @@ cd "./$1"
 shift
 ROOT="`pwd`"
 
-TAG=1f1ba06aa2a83a2c75ade4150b7c0bba10531088
+TAG=1dcaea8b601aa969ffd5bfa70088957ce3eaa273
 if [ ! -d "libplacebo" ]; then
 git clone --recursive https://github.com/haasn/libplacebo.git || exit 1
 fi
 cd libplacebo || exit 1
 git checkout $TAG || exit 1
-git apply "${SCRIPT_DIR}/flatpak/0001-Vulkan-Don-t-try-to-reuse-old-swapchain.patch" || exit 1
-git apply "${SCRIPT_DIR}/flatpak/0002-Vulkan-use-16bit-for-p010.patch" | exit 1
 DIR=./build || exit 1
 meson setup --prefix /usr -Dxxhash=disabled $DIR || exit 1
 ninja -C$DIR || exit 1
