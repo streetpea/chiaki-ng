45f7eca951179fc2d2ef5bf55274fa369307210f
Street Pea <streetpea@proton.me>
2026-04-01 14:16:18 -0700

    Add configurable libplacebo tag

--- scripts/build-libplacebo-windows.sh
+++ scripts/build-libplacebo-windows.sh
@@ -6,7 +6,7 @@ cd "./$1"
 shift
 ROOT="`pwd`"
 
-TAG=v7.360.1
+TAG="${LIBPLACEBO_VERSION:-v7.360.1}"
 if [ ! -d "libplacebo" ]; then
 git clone --recursive https://github.com/haasn/libplacebo.git || exit 1
 fi
